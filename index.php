2ecd9da04a0c234acfd2bb0a7668ee6ef34f7b5b
NaveenDK <naveenkolambage@gmail.com>
2019-08-18 14:07:06 +1200

    added the navigation bar-styling

--- index.php
+++ index.php
@@ -26,8 +26,30 @@ include("includes/config.php");
  			<nav class="navBar">
 
  				<a href="index.php" class="logo">
-					 <img class="logo-size"src="assets/images/icons/logo-test.png">
+					 <img src="assets/images/icons/logo-test.png">
 				 </a>
+ 			<div class="group">
+				 <div class="navItem">
+					 <a href="search.php" class="navItemLink">Search 
+						 <img src="assets/images/icons/search.png" class="icon" alt="Search">
+					 </a>
+				</div>
+			</div>
+			
+			<div class="group">
+				<div class="navItem">
+					 <a href="browse.php" class="navItemLink"> Browse</a>
+				</div>
+				<div class="navItem">
+					 <a href="yourMusic.php" class="navItemLink">Your Music </a>
+				</div>
+				<div class="navItem">
+					 <a href="profile.php" class="navItemLink">B C G Guy</a>
+				</div>
+			
+			</div>
+
+
 
 			 </nav>
 		</div>
