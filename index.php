7a1fcb40f3f28cc6e67cd518ea8220fa0e131d06
NaveenDK <naveenkolambage@gmail.com>
2019-08-19 04:47:45 +1200

    Seperated sections, containers from the index file and included it seperately - clean and organized structure

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<?php  
-include("includes/config.php");
+<?php include ("includes/header.php") ?>
 
-//session_destroy();
- if(isset($_SESSION['userLoggedIn'])){
-	 $userLoggedIn= $_SESSION['userLoggedIn'];
-	 //echo $userLoggedIn;
- }
- else{
-	 header("Location:register.php");
- }
-?>
-
-<html>
-<head>
-	<title>Welcome to Slotify!</title>
-
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-
-<body>
-
-<div id="mainContainer">
-	<div id="topContainer">
- 	 <?php include("includes/navBarContainer.php"); ?>
-	</div>
-
-    <?php include("includes/nowPlayingBar.php"); ?>
-
-</div>
-
-
-
-</body>
-
-</html>
+<?php include ("includes/footer.php") ?>
+	
