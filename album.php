f54813757c16edd1b90adf073627bcb506c881cd
NaveenDK <naveenkolambage@gmail.com>
2019-08-24 16:26:09 +1200

    Added the Artist Query to pick up artist via album id

--- album.php
+++ album.php
@@ -1,7 +1,10 @@
 <?php include ("includes/header.php");
 if(isset($_GET['id'])){
    $albumId = $_GET['id'];
+   //echo $albumId;'
 }
+
+
 else{
     header("Location:index.php"); //Redirect to index page
 }
@@ -9,13 +12,15 @@ else{
 $albumQuery = mysqli_query($con,"SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-$artistId = $album['artist']
+$artistId = $album['artist'];
+//echo $artistId;
 
 $artistQuery = mysqli_query($con,"SELECT * FROM artists WHERE id='$artistId'");
 $artist = mysqli_fetch_array($artistQuery);
 
 echo $album['title']."<br>";
 echo $artist['name'];
+
 ?>
 
 <?php include("includes/footer.php"); ?>
