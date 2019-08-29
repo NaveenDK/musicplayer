4bf6b3a17fa213ae5438b8054e839214e5f4a973
NaveenDK <naveenkolambage@gmail.com>
2019-08-30 01:05:53 +1200

    formating the time label - adding an extraZero to instances where time is less than 10 seconds for example 2:4sec will be 2:04sec

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds){
+  var time = Math.round(seconds);
+  var minutes  = Math.floor(time/60);
+  var seconds  = time- (minutes*60);
+
+  var extraZero;
+
+  // if(seconds<10){
+  //   extraZero = "0";
+  // }else{
+  //   extraZero = "";
+  // }
+
+  var extraZero = (seconds<10) ? "0":"";
+
+  return minutes + ":" + extraZero +  seconds;
+}
+
 function Audio() {
   this.currentlyPlaying;
   this.audio = document.createElement("audio");
 
  this.audio.addEventListener("canplay",function(){
    //this refers to the object that the event was called on
-   $(".progressTime.remaining").text(this.duration);
+   var duration = formatTime(this.duration);
+      $(".progressTime.remaining").text(duration);
  });
- //this.audio.d uration here is same as this.duration above
+ //this.audio.duration here is same as this.duration above
 
   this.setTrack = function(track) {
     this.currentlyPlaying = track;
