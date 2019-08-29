5b0b79e8505eb98dee1092a6306b96c722f46669
NaveenDK <naveenkolambage@gmail.com>
2019-08-30 00:54:21 +1200

    added a eventlistener function to update the progress time remaining label

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ var audioElement;
 function Audio() {
   this.currentlyPlaying;
   this.audio = document.createElement("audio");
+
+ this.audio.addEventListener("canplay",function(){
+   //this refers to the object that the event was called on
+   $(".progressTime.remaining").text(this.duration);
+ });
+ //this.audio.duration here is same as this.duration above
+
   this.setTrack = function(track) {
     this.currentlyPlaying = track;
     this.audio.src = track.path;
