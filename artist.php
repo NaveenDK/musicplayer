c73b37bb7320ff55d7e58a41d5ff007e38a6adcf
NaveenDK <naveenkolambage@gmail.com>
2019-09-01 11:32:52 +1200

    create an artist section easily (copying album) modify existing query -limit songs to 5 on the artist page + add borderBottom style

--- artist.php
+++ artist.php
@@ -14,7 +14,7 @@ $artist = new Artist($con,$artistId);
 
 ?>
 
-<div class="entityInfo">
+<div class="entityInfo borderBottom">
     <div class="centerSection">
         <div class="artistInfo">
 
@@ -32,3 +32,58 @@ $artist = new Artist($con,$artistId);
     </div>
 
 </div>
+
+<div class="tracklistContainer borderBottom">
+    <ul class="tracklist">
+        <?php
+        $songIdArray = $artist->getSongIds();
+        $i=1;
+        foreach($songIdArray as $songId){
+          //  echo $songId."<br>";
+            if($i>5){
+                break;
+            }
+
+          $albumSong = new Song($con,$songId);
+          $albumArtist = $albumSong ->getArtist();
+         // echo $albumSong->getTitle();
+          echo "<li class='tracklistRow '>
+                <div class='trackCount'>
+                    <img class='play' src = 'assets/images/icons/play-white.png' onclick='setTrack(\"".$albumSong->getId()."\", tempPlaylist,true)'>
+                    <span class='trackNumber'>$i </span>
+                </div>
+
+                <div class='trackInfo'>
+                  <span class='trackName'>". $albumSong->getTitle()."</span>
+                  <span class='artistName'>".$albumArtist ->getName()."</span>
+                </div>
+
+                <div class='trackOptions'>
+                    <img class='optionsButton' src='assets/images/icons/more.png'>
+                </div>
+
+                <div class='trackDuration'>
+                    <span class='duration'>" . $albumSong-> getDuration() . "</span>
+                </div>
+          </li>";
+
+          $i=$i+1;
+
+
+         // $albumSong->getTitle();
+
+        }
+        ?>
+
+        <script>
+            var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+            tempPlaylist = JSON.parse(tempSongIds);
+
+
+        </script>
+
+
+    </ul>
+
+</div>
+
--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -17,4 +17,16 @@ class Artist{
 
     }
 
+    public function getSongIds(){
+        $query = mysqli_query($this->con,"SELECT id FROM songs WHERE artist='$this->id' ORDER BY plays DESC");
+  
+        $array = array();
+  
+        while($row= mysqli_fetch_array($query)){
+          array_push($array,$row['id']);
+        }
+        return $array;
+      }
+      
+
 }
