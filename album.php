c425e15473930cc3b6cca230e99573920f2c7ed7
NaveenDK <naveenkolambage@gmail.com>
2019-09-01 23:19:35 +1200

    add styling fo option menu

--- album.php
+++ album.php
@@ -88,3 +88,9 @@ $artist = $album->getArtist();
 
 </div>
 
+<nav class="optionsMenu">
+  <input type="hidden" class="songId">
+    <div class="item">Add to playlist</div>
+    <div class="item">Item 2</div>
+    <div class="item">Item 3</div>
+</nav>
